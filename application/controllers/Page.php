8c87ba848adcd1ca009a0c102248f4ffa02c1230
Prabodhan Sonawane <prabodhan.s29@gmail.com>
2023-11-16 21:54:21 +0530

    frontend completed

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -2,38 +2,37 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Page extends CI_Controller {
-    function about() {
+    public function about() {
         $this->load->view('front/about');
     }
 
-    function services() {
+    public function services() {
         $this->load->view('front/services');
     }
 
-    function contact() {
+    public function contact() {
         $this->load->library('form_validation');
         $this->form_validation->set_rules('name', 'Name', 'required');
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
         $this->form_validation->set_error_delimiters('<p class="invalid-feedback">', '</p>');
 
         if ($this->form_validation->run() == true) {
-            // Here we will process contact us form
-
+            // Email functionality
             $config = array(
                 'protocol' => 'smtp',
                 'smtp_host' => 'ssl://smtp.gmail.com',
                 'smtp_port' => 465,
-                'smtp_user' => 'prabodhan.s29@gmail.com', // please replace this with yours 
-                'smtp_pass' => 'Prabodhan12345!@#', // please replace this with yours
+                'smtp_user' => '', // replace this with your email id
+                'smtp_pass' => '******', // replace this with your password
                 'mailtype'  => 'html',
                 'charset'   => 'iso-8859-1'
             );
             $this->load->library('email', $config);
             $this->email->set_newline("\r\n");
 
-            $this->email->to('prabodhan.s29@gmail.com'); // This is an email where you want to receive email
-            $this->email->from('prabodhan.s29@gmail.com'); // This is from email address 
-            $this->email->subject('You have received an enquiry'); // This is email subject
+            $this->email->to(''); // receiver's email
+            $this->email->from('test@test.com'); // sender's address
+            $this->email->subject('You have received an enquiry'); // email subject
 
             $name = $this->input->post('name');
             $email = $this->input->post('email');
@@ -50,7 +49,6 @@ class Page extends CI_Controller {
             $message .= "Email Example by Prabodhan";
 
             $this->email->message($message);
-
             $this->email->send();
 
             $this->session->set_flashdata('msg', 'Thanks for your enquiry, we will get back to you soon.');
